abd82f677e082e100fb67057702e72d74cc59668
Kyle Ortiz <kyleaortiz@gmail.com>
2021-10-07 20:25:45 -0500

    commit tree b210852e8f0c2828fe842738e1d79c0c870f2a90

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,19 @@
 class BirdsController < ApplicationController
+  def update
+    bird = Bird.find_by(id: params[:id])
+    bird.update(bird_params)
+    render json: bird 
+  end
 
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end 
   # GET /birds
   def index
     birds = Bird.all
@@ -25,7 +39,7 @@ class BirdsController < ApplicationController
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
